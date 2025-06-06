04068f2307171394d865f44c0dfe5a19591181a4
ikrivezhenko <147913647+ikrivezhenko@users.noreply.github.com>
2025-06-06 08:23:14 +0700

    Delete Internet-Market/Internet-Market directory

--- Internet-Market/Internet-Market/Auth-Service/src/main/java/com/example/spring/auth/dto/JwtRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring.auth.dto;
-
-import lombok.Data;
-
-@Data
-public class JwtRequest {
-    private String username;
-    private String password;
-}
--- Internet-Market/Internet-Market/flyway/start-migration.bat
+++
@@ -1,6 +0,0 @@
-cd products
-start cmd /k docker-compose up
-cd ../users
-start cmd /k docker-compose up
-cd ../analit
-start cmd /k docker-compose up
